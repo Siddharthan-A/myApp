2771fef4dc64d85aac44ee196b66a40f33ac0f5b
Siddharthan-A <siddharthan.anbazhagan@gmail.com>
2025-09-13 21:20:42 +0530

    Added new changes

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello from DevOps Pipeline!"
+def hello():
+    return "Hello, DevOps World!"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
